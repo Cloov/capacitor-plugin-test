c1be05bb1cac33e8cd063e435e1683d75a8c93e6
Shaun Campbell <industrion@gmail.com>
2019-09-20 14:51:24 +0100

    Added test plugin and made an attempt to call it in home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Plugins } from '@capacitor/core';
 
 @Component({
   selector: 'page-home',
@@ -8,6 +9,12 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
+    let args = { value: "Hello" };
+    Plugins.Concatenate.echo(args).then(result => {
+      console.log("Concatenate.echo() result: ", result);
+    }, error => {
+      console.log("Concatenate.echo() error: ", error);
+    });
 
   }
 
