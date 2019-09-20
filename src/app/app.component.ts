9bb685b7de1e5b598161c170e1f15023832c5565
Shaun Campbell <industrion@gmail.com>
2019-09-20 15:12:46 +0100

    Added Concatenate plugin 'registerWebPlugin' in app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,10 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
 import { HomePage } from '../pages/home/home';
+import { Concatenate } from 'concatenate';
+import { registerWebPlugin } from '@capacitor/core';
+
 @Component({
   templateUrl: 'app.html'
 })
@@ -11,6 +13,9 @@ export class MyApp {
   rootPage:any = HomePage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+
+    registerWebPlugin(Concatenate);
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
